1ddae29bbd4a38a55de4257b114f29ecd307d7cc
Diogo Biazus <diogob@gmail.com>
2014-10-31 17:22:35 -0400

    making specs simpler, gave up the given/when/then syntax. Let's stick to good old jasmine :P

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 #jquery.fixedmask [![Build Status](https://travis-ci.org/diogob/jquery.fixedmask.svg?branch=master)](https://travis-ci.org/diogob/jquery.fixedmask) [![Code Climate](https://codeclimate.com/github/diogob/jquery.fixedmask/badges/gpa.svg)](https://codeclimate.com/github/diogob/jquery.fixedmask)
 
-================
 
 JQuery plugin for fixed size input mask
--- spec/setup.spec.js
+++ spec/setup.spec.js
@@ -1,33 +1,21 @@
-feature("Masking an Input", function() {	
-  describe("#fixedMask", function(){
-    var applyMask;
-    beforeEach(function(){
-      applyMask = input.fixedMask("9.9.9-9").data('applyMask');
-    });
-
-    it("should create fixedMaskFunctions in input data", function(){
-      expect(applyMask).toEqual(jasmine.any(Function));
-    })
-
-    it("the applyMask should work", function(){
-      expect(applyMask('1', '1')).toEqual('1.');
-      expect(applyMask('1.2', '1')).toEqual('1.2.');
-      expect(applyMask('1.2.3', '1')).toEqual('1.2.3-');
-    });
+describe("Apply forward mask", function(){
+  var applyMask;
+  beforeEach(function(){
+    input.val('').fixedMask("9.9.9-9");
   });
 
-	scenario('Applying a mask to an input',function(){
-		given("an input with two masks", function(){
-			input.fixedMask("9.9.9-9");
-		});
+  it("should apply mask when inputting 2 digits", function(){
+    input.mashKeys("12");
+    expect(input).toHaveValue('1.2');
+  });
 
-		when("typing a number",function(){
-			input.mashKeys("11");
-		});
+  it("should apply mask when inputting 3 digits", function(){
+    input.mashKeys("123");
+    expect(input).toHaveValue('1.2.3');
+  });
 
-		then("value should be correct",function(){
-			expect(input).toHaveValue('1.1');
-		});
-	});
+  it("should apply mask when inputting 4 digits", function(){
+    input.mashKeys("1234");
+    expect(input).toHaveValue('1.2.3-4');
+  });
 });
-
